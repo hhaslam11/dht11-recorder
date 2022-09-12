800d876b168387d7a17550575cbd5d6ec04cfb8d
Kaleb Haslam <kalebmarc@hotmail.com>
2022-09-11 20:32:08 -0700

    fake dht11 sensor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { InfluxDB, Point } = require('@influxdata/influxdb-client');
+const dht11 = require("node-dht-sensor").promises;
 require('dotenv').config();
 
 // You can generate a Token from the "Tokens Tab" in the UI
@@ -7,20 +8,33 @@ const org = process.env.ORG;
 const bucket = process.env.BUCKET;
 
 const client = new InfluxDB({ url: process.env.URL, token: token });
+dht11.initialize({
+  test: {
+    fake: {
+      temperature: 21,
+      humidity: 60
+    }
+  }
+});
 
 const writeApi = client.getWriteApi(org, bucket);
 writeApi.useDefaultTags({ host: 'host1' });
 
 const mockData = () => {
-  const temp = new Point('sensors')
-    .floatField('temp', Math.floor(Math.random() * 37));
+
+  // fetch temp and humidity from dht11
+  dht11.read(22, 4).then(res => {
+    const temp = new Point('sensors')
+    .floatField('temp', res.temperature.toFixed(2));
     
-  const hum = new Point('sensors')
-    .floatField('humidity', Math.floor(Math.random() * 100));
-  
-  writeApi.writePoint(temp);
-  writeApi.writePoint(hum);
-  console.log(`setting point (${temp}; ${hum}`);
+    const hum = new Point('sensors')
+      .floatField('hum', res.humidity.toFixed(2));
+    
+    writeApi.writePoint(temp);
+    writeApi.writePoint(hum);
+    console.log(`setting point (${temp}; ${hum}`);
+  }).catch(console.err);
+
 };
 
-setInterval(mockData, 1000);
+setInterval(mockData, 5000);
